45c9ead1577026507ac096cc4684a58f3363cb18
Prakash1154 <prakashraj7325@gmail.com>
2019-12-14 18:01:58 +0530

    Updates

---
+++ precache-manifest.90f5d78cb1914a2a4f9cfab00583e4ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f4bc98816641c6b01f523ef35142df",
+    "url": "/FaceDetectionUsingReact/index.html"
+  },
+  {
+    "revision": "370a22cefee16432acfd",
+    "url": "/FaceDetectionUsingReact/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fb847e1a2de4ab435b32",
+    "url": "/FaceDetectionUsingReact/static/css/main.4d0af125.chunk.css"
+  },
+  {
+    "revision": "370a22cefee16432acfd",
+    "url": "/FaceDetectionUsingReact/static/js/2.c64cf13d.chunk.js"
+  },
+  {
+    "revision": "fb847e1a2de4ab435b32",
+    "url": "/FaceDetectionUsingReact/static/js/main.ed9a8326.chunk.js"
+  },
+  {
+    "revision": "53dbddcd69e1237fcf67",
+    "url": "/FaceDetectionUsingReact/static/js/runtime-main.ab9499b1.js"
+  },
+  {
+    "revision": "a0d7f069f688d308dd15580d0df7a4f2",
+    "url": "/FaceDetectionUsingReact/static/media/brain.a0d7f069.png"
+  }
+]);
